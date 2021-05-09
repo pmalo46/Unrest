3d60091635230d6278a0b79c5061b67ac70af1d1
Patrick Maloney <pmaloney046@gmail.com>
2021-05-08 20:18:50 -0400

    update SHiny app

--- UnrestPredictor/app.R
+++ UnrestPredictor/app.R
@@ -4,7 +4,7 @@ library(DT)
 
 shinyApp(
     ui = navbarPage("Testing the saving to dataframes",
-                    tabPanel("Plot",
+                    tabPanel("Predition",
                              
                              mainPanel(
                                  numericInput("month", "Month",value = 1, min = 1, max = 12, step = 1),
@@ -54,12 +54,45 @@ shinyApp(
                                                "Disruptive State Acts" = "4",
                                                "Political Reconfigurations" = "5")),
                                  numericInput("n_killed_a", "Number of Fatalities",value = 0, min = 0, step = 1),
-                                 selectInput("know_ini", "Is the Initiator of the event known?",
+                                 selectInput("know_ini", "Is the Initiator (individual or group) of the event known?",
                                              c("Initiator Unknown" = "0",
                                                "Initiator Known" = "1",
                                                "Initiator Ambiguous" = "2",
                                                "Initiator Suspected" = "5")),
-                                 sliderInput('chanceofsale',"Probability of Sale", min=1, max=10,value=5),
+                                 selectInput("weapon", "Type of Weapon Used",
+                                             c("No Weapon Used" = '1',
+                                               "Fake weapon Used" = '2',
+                                               "Body Parts" = '3',
+                                               "Animal" = '4',
+                                               "Vehicles" = '5',
+                                               "Computer" = '6',
+                                               "Blunt Instrument" = '7',
+                                               "Tear gas, Mace, etc." = '8',
+                                               "Knives/sharp Instrument" = '9',
+                                               "IED" = '10',
+                                               "Letter Bomb" = '11',
+                                               "Fire" = '12',
+                                               "Non-lethal Projectiles" = '13',
+                                               "Small Arms" = '14',
+                                               "Light Weapon" = '15',
+                                               "Incendiary Device" = '16',
+                                               "Land Mine" = '17',
+                                               "Explosives, Grenade" = '18',
+                                               "Car Bomb" = '19',
+                                               "Tanks/armored Vehicles" = '20',
+                                               "Field Artillery" = '21',
+                                               "Missile/rocket" = '22',
+                                               "Aircraft Munitions" = '23',
+                                               "Naval Power" = '24',
+                                               "Biochemical Weapons" = '25',
+                                               "Unspecified" = '26',
+                                               "Other weapon" = '27')),
+                                 selectInput("sc_animosity", "Socio-cultural Animosity",
+                                             c("True" = TRUE,
+                                               "False" = FALSE)),
+                                 selectInput("anti_gov_sentmnts", "Anti-government sentiments",
+                                             c("True" = TRUE,
+                                               "False" = FALSE)),
                                  actionButton("submit", ("Submit"))
                              )
                              
@@ -67,7 +100,7 @@ shinyApp(
                     tabPanel("Table",
                              
                              "Testing Table",
-                             DT::dataTableOutput("Test_Table"),
+                             DT::dataTableOutput("table"),
                              hr(),
                              hr(),
                              "Combined Table",
@@ -77,20 +110,20 @@ shinyApp(
     
     server = function(input, output) {
         
-        #Testdata <- data.frame("month" = "month", "week" = "week",
-                            #   "region" = "region", "chanceofsale" = "5")
-        
+        new_event <- reactive({ data.frame("month" = factor(input$month), "week" = factor(input$week),
+                                "region" = factor(input$region), "loc_type" = factor(input$loc_type),
+                                "ev_type" = factor(input$ev_type), "n_killed_a" = factor(input$n_killed_a),
+                                "know_ini" = factor(input$know_ini), "weapon" = factor(input$weapon),
+                                "sc_animosity" = input$sc_animosity, "anti_gov_sentmnts" = input$anti_gov_sentmnts)})
+       
         #Initial Dataframe
-        FinalData = eventReactive(input$submit,{
-            Testdata = data.frame("month" = input$month, "week" = input$week,
-                                        "region" = input$region,
-                                        "chanceofsale" = as.character(input$chanceofsale))
+        pred = eventReactive(input$submit,{
+            new_event
+            print(str(new_event))
         })
         
-        #Initial Table
-        
-        #Combined Table
-        output$Combined_table = renderDataTable(FinalData())
+        #Table
+        output$table = renderDataTable(pred())
         
     }#Server End
 )
